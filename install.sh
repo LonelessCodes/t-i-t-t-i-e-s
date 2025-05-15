75375c3818328941dd747db523b5f5d7c8e88c1a
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2025-05-15 03:58:26 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln titties-on-boot.service /etc/systemd/system/titties-on-boot.service
+
+systemctl daemon-reload
+systemctl enable titties-on-boot.service
+systemctl start titties-on-boot.service
+
+echo "Titties on boot service installed and started."
+echo "You can check the status of the service with:"
+echo "systemctl status titties-on-boot.service"

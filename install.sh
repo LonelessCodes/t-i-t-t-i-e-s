c1b7899306fd8c52cb05781d2f6e4f78db5442b7
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2025-05-28 16:18:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 ln titties-on-boot.service /etc/systemd/system/titties-on-boot.service
 
+systemctl stop titties-on-boot.service
+systemctl disable titties-on-boot.service
 systemctl daemon-reload
 systemctl enable titties-on-boot.service
 systemctl start titties-on-boot.service
